12d0fde6b1c94d5e2cd5c65f95e8d63636a9b567
Sahar Moalem <saharmoalem333@gmail.com>
2021-09-18 23:45:55 +0300

    F

---
+++ MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IWebHostEnvironment Environment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment _environment)
+        {
+            _logger = logger;
+            Environment = _environment;
+        }
+
+        public IActionResult Index()
+        {
+            string wwwPath = this.Environment.WebRootPath;
+            string contentPath = this.Environment.ContentRootPath;
+
+            return View();
+        }
+
+        public IActionResult Get()
+        {
+            string path = Path.Combine(Environment.WebRootPath, "MyPhoto.jpg");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcMovie/README.md
@@ -0,0 +1,2 @@
+This is my first ASP.NET MVC project.
+In my project website, I share some information about myself, and display a database that features PS5 games that I love and wish to buy.
